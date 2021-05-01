84255f4a397a00c226d705e36a0507ee9f2abc6c
DougGoncalves <doug.goncalvesf@gmail.com>
2021-05-01 12:58:53 -0300

    First commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Nav, NavbarContainer, NavLogo } from './NavBarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo>
+            Dinheirinho
+          </NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
+
